6355680cf75592cb380e2918a42de1c963a87280
Paul Lensing <paul.lensing@gmail.com>
2014-05-21 12:02:54 -0700

    Moved mounting operation from main to hflat_init this solves problems when mounting the file system without the -f foreground option.

--- src/main.cc
+++ src/main.cc
@@ -21,6 +21,85 @@
 #include "simple_kinetic_namespace.h"
 #include "distributed_kinetic_namespace.h"
 #include <libconfig.h>
+#include <glog/logging.h>
+#include <unistd.h>
+
+
+static struct fuse_operations hflat_ops;
+static std::string filename;
+
+static bool parse_configuration(
+        std::vector< hflat::Partition > &clustermap, hflat::Partition &logpartition,
+        int &cache_expiration_ms, PosixMode &pmode)
+{
+    auto cfg_to_hflat = [&](config_setting_t *partition, hflat::Partition &p) -> bool {
+        if(partition)
+        for(int i=0; i<config_setting_length(partition); i++){
+              config_setting_t *drive = config_setting_get_elem(partition, i);
+              const char *host, *status; int port;
+
+              if( !config_setting_lookup_string(drive, "host",   &host) ||
+                  !config_setting_lookup_string(drive, "status", &status) ||
+                  !config_setting_lookup_int   (drive, "port",   &port) )
+                  return false;
+
+              hflat::KineticDrive *d = p.mutable_drives()->Add();
+              d->set_host(host);
+              d->set_port(port);
+
+              if(strcmp(status,"GREEN") == 0)
+                  d->set_status(hflat::KineticDrive_Status_GREEN);
+              else if(strcmp(status,"YELLOW") == 0)
+                  d->set_status(hflat::KineticDrive_Status_YELLOW);
+              else if(strcmp(status,"RED") == 0)
+                  d->set_status(hflat::KineticDrive_Status_RED);
+              else
+                  return false;
+
+        }
+        return true;
+    };
+
+
+    config_t cfg;
+    config_init(&cfg);
+    bool rtn = config_read_file(&cfg, filename.c_str());
+    if  (rtn) rtn = cfg_to_hflat(config_lookup(&cfg, "log"), logpartition);
+
+    if( config_setting_t * cmap = config_lookup(&cfg, "clustermap")) {
+        for(int i = 0; rtn && i < config_setting_length(cmap); i++){
+            hflat::Partition p;
+            rtn = cfg_to_hflat(config_setting_get_elem(cmap, i), p);
+
+            /* Special case: 2 drives in partition. We add a logdrive to the partition to enable the namespace to
+             * differentiate between a network split and a failed drive. */
+            if(p.drives_size() == 2 && logpartition.drives_size())
+                p.set_logid(i % logpartition.drives_size());
+
+            p.set_partitionid(i);
+            clustermap.push_back(p);
+        }
+    }
+
+
+    if (config_setting_t * options =  config_lookup(&cfg, "options")){
+        config_setting_lookup_int(options, "cache_expiration", &cache_expiration_ms);
+
+        const char *mode;
+        if( config_setting_lookup_string(options, "posix_mode", &mode) )
+            if(strcmp(mode,"RELAXED") == 0)
+                pmode = PosixMode::TIMERELAXED;
+    }
+
+
+    if(rtn == false)
+        hflat_error("Error in configuration file %s: %s:%d - %s\n", filename.c_str(),
+                config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
+    config_destroy(&cfg);
+    return rtn;
+}
+
+
 
 /**
  * Initialize filesystem
@@ -34,6 +113,37 @@
  */
 void *hflat_init(struct fuse_conn_info *conn)
 {
+    struct hflat_priv *priv = 0;
+    std::vector< hflat::Partition > clustermap;
+    hflat::Partition logpartition;
+    PosixMode mode = PosixMode::FULL;
+    int cache_expiration_ms = 1000;
+
+    if(! filename.empty()){
+        bool cok = parse_configuration(
+                        clustermap, logpartition,
+                        cache_expiration_ms,
+                        mode);
+        if(cok == false) REQ(EXIT_FAILURE);
+    }
+
+    try {
+        if(clustermap.empty())
+            priv = new hflat_priv(new SimpleKineticNamespace(), cache_expiration_ms, 1024*1024, mode);
+        else if(clustermap.size() == 1 && clustermap.at(0).drives_size() == 1){
+            hflat_debug("simple namespace used");
+            priv = new hflat_priv(new SimpleKineticNamespace(clustermap[0].drives(0)), cache_expiration_ms, 1024*1024, mode);
+        }
+        else
+            priv = new hflat_priv(new DistributedKineticNamespace(clustermap, logpartition), cache_expiration_ms, 1024*1024, mode);
+    }
+    catch(std::exception& e){
+        hflat_error("Exception thrown during mount operation. Reason: %s \n Check your Configuration.",e.what());
+        REQ(EXIT_FAILURE);
+    }
+    fuse_get_context()->private_data = priv;
+
+
     /* Setup values required for inode generation. */
     if ( util::grab_inode_generation_token() )
         hflat_error("Error encountered during setup of inode number generation");
@@ -65,7 +175,8 @@ void hflat_destroy(void *priv)
     delete PRIV;
 }
 
-static struct fuse_operations hflat_ops;
+
+
 static void init_hflat_ops(fuse_operations *ops)
 {
     ops->create = hflat_fcreate;
@@ -118,119 +229,35 @@ static void init_hflat_ops(fuse_operations *ops)
     ops->destroy = hflat_destroy;
 }
 
-
-bool parse_configuration(char *file,
-        std::vector< hflat::Partition > &clustermap, hflat::Partition &logpartition,
-        int &cache_expiration_ms, PosixMode &pmode)
-{
-    auto cfg_to_hflat = [&](config_setting_t *partition, hflat::Partition &p) -> bool {
-        if(partition)
-        for(int i=0; i<config_setting_length(partition); i++){
-              config_setting_t *drive = config_setting_get_elem(partition, i);
-              const char *host, *status; int port;
-
-              if( !config_setting_lookup_string(drive, "host",   &host) ||
-                  !config_setting_lookup_string(drive, "status", &status) ||
-                  !config_setting_lookup_int   (drive, "port",   &port) )
-                  return false;
-
-              hflat::KineticDrive *d = p.mutable_drives()->Add();
-              d->set_host(host);
-              d->set_port(port);
-
-              if(strcmp(status,"GREEN") == 0)
-                  d->set_status(hflat::KineticDrive_Status_GREEN);
-              else if(strcmp(status,"YELLOW") == 0)
-                  d->set_status(hflat::KineticDrive_Status_YELLOW);
-              else if(strcmp(status,"RED") == 0)
-                  d->set_status(hflat::KineticDrive_Status_RED);
-              else
-                  return false;
-
-        }
-        return true;
-    };
-
-
-    config_t cfg;
-    config_init(&cfg);
-    bool rtn = config_read_file(&cfg, file);
-    if  (rtn) rtn = cfg_to_hflat(config_lookup(&cfg, "log"), logpartition);
-
-    if( config_setting_t * cmap = config_lookup(&cfg, "clustermap")) {
-        for(int i = 0; rtn && i < config_setting_length(cmap); i++){
-            hflat::Partition p;
-            rtn = cfg_to_hflat(config_setting_get_elem(cmap, i), p);
-
-            /* Special case: 2 drives in partition. We add a logdrive to the partition to enable the namespace to
-             * differentiate between a network split and a failed drive. */
-            if(p.drives_size() == 2 && logpartition.drives_size())
-                p.set_logid(i % logpartition.drives_size());
-
-            p.set_partitionid(i);
-            clustermap.push_back(p);
-        }
-    }
-
-
-    if (config_setting_t * options =  config_lookup(&cfg, "options")){
-        config_setting_lookup_int(options, "cache_expiration", &cache_expiration_ms);
-
-        const char *mode;
-        if( config_setting_lookup_string(options, "posix_mode", &mode) )
-            if(strcmp(mode,"RELAXED") == 0)
-                pmode = PosixMode::TIMERELAXED;
-    }
-
-
-    if(rtn == false)
-        hflat_error("Error in configuration file %s: %s:%d - %s\n", file,
-                config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
-    config_destroy(&cfg);
-    return rtn;
-}
-
-
+#include <sys/param.h>
 int main(int argc, char *argv[])
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    struct hflat_priv *priv = 0;
 
-    std::vector< hflat::Partition > clustermap;
-    hflat::Partition logpartition;
-    int cache_expiration_ms = 1000;
-    PosixMode mode = PosixMode::FULL;
+    /* google logging by kinetic-cpp-client */
+    google::InitGoogleLogging(argv[0]);
 
     init_hflat_ops(&hflat_ops);
 
-    for(int i=0; i<argc; i++)
+    /* extract cfg file path */
+    for(int i=0; i<argc; i++){
         if(strncmp(argv[i],"-cfg=",5) == 0){
-            if( false == parse_configuration(argv[i]+5,
-                    clustermap, logpartition,
-                    cache_expiration_ms,
-                    mode))
-                return(EXIT_FAILURE);
-            else{
-                for(int j=i+1; j<argc; j++)
-                    argv[j-1] = argv[j];
-                argc--;
-            }
+            filename = std::string(argv[i]+5);
+            for(int j=i+1; j<argc; j++)
+                argv[j-1] = argv[j];
+            argc--;
         }
+    }
 
-    try {
-        if(clustermap.empty())
-            priv = new hflat_priv(new SimpleKineticNamespace(), cache_expiration_ms, 1024*1024, mode);
-        else if(clustermap.size() == 1 && clustermap.at(0).drives_size() == 1){
-            hflat_debug("simple namespace used");
-            priv = new hflat_priv(new SimpleKineticNamespace(clustermap[0].drives(0)), cache_expiration_ms, 1024*1024, mode);
-        }
-        else
-            priv = new hflat_priv(new DistributedKineticNamespace(clustermap, logpartition), cache_expiration_ms, 1024*1024, mode);
+    /* transform relative to absolute path, as fuse will switch
+     * working directory to root before calling init. */
+    if(filename.length() && filename[0]!='/'){
+        char buf[PATH_MAX];
+        getcwd(buf, PATH_MAX);
+        filename.insert(0,"/");
+        filename.insert(0,buf);
+        hflat_trace("using configuration file %s",filename.c_str());
     }
-    catch(std::exception& e){
-        hflat_error("Exception thrown during mount operation. Reason: %s \n Check your Configuration.",e.what());
-        return(EXIT_FAILURE);
-    }
-    hflat_debug("Read cache expiration time set to %d milliseconds. Posix mode is %s", cache_expiration_ms, mode == PosixMode::FULL ? "FULL" : "RELAXED" );
-    return fuse_main(argc, argv, &hflat_ops, (void*)priv);
+
+    return fuse_main(argc, argv, &hflat_ops, nullptr);
 }
